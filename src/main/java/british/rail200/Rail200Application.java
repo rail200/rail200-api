18284bfa3713ed9ef92102c453e9078e15096bb7
Rail200 <british.rail.200@gmail.com>
2024-07-13 20:12:20 +0200

    Demo project pushed

---
+++ src/main/java/british/rail200/Rail200Application.java
@@ -0,0 +1,13 @@
+package british.rail200;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rail200Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rail200Application.class, args);
+	}
+
+}
---
+++ src/test/java/british/rail200/Rail200ApplicationTests.java
@@ -0,0 +1,13 @@
+package british.rail200;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rail200ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
